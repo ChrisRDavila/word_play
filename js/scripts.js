2436ddba17802400948a6cf0448d45744d1f1dc2
ChrisRDavila <crossdavila@gmail.com>
2023-09-02 21:34:47 -0700

    will gather input, create array, split, reverse and reform new sentence with words over 3 letters long, still want to find most feesable way to reset page so when submit only shows most resent results, figuring out best way to approach due to createElement method of printing new input

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// User Interface Logic
+function handleForm(event) {
+  event.preventDefault();
+  let inputSentence = document.querySelector("input").value;
+  let sentenceArray = inputSentence.split(" ");
+
+
+  // create results heading
+  const displayHeading = document.createElement("h2");
+  displayHeading.append("Here is your new sentence:");
+  document.querySelector("form#sentence").reset();
+  document.body.append(displayHeading);
+  filterSentence = [];
+
+
+  sentenceArray.forEach(function(element) {
+    
+    if (element.length >= 3) {
+      filterSentence.push(element);
+      console.log(filterSentence); 
+    }
+  });  
+    let revArray = filterSentence.reverse();
+    console.log(revArray);
+    let newSentence = revArray.join(" ");
+    console.log(newSentence);
+    const paragraph = document.createElement("p");
+    paragraph.append(newSentence);
+    document.body.append(paragraph);
+}
+
+
+window.addEventListener("load", function() {
+  document.querySelector("form#sentence").addEventListener("submit", handleForm);
+});
